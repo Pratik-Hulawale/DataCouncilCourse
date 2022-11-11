dc2b9e6075c86ebab779f72e0d1ca9c7f6768c87
pratik-1847 <83464018+pratik-1847@users.noreply.github.com>
2022-11-11 14:48:20 +0530

    signal app

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-let a= prompt("enter your age")
-if(a>=18){
-    alert("You are eligible for voting")
-}else{
-    alert("you are not eligible")
-}
+/* Buttons */
+let red1=document.querySelector(".red-btn")
+let yellow=document.querySelector(".yellow-btn")
+let green=document.querySelector(".green-btn")
+
+//Colors 
+let redc=document.querySelector(".noactive1")
+let greenc=document.querySelector(".noactive2")
+let yellowc=document.querySelector(".noactive3")
+
+red1.addEventListener('click',function(){
+    redc.classList.add("red");
+    greenc.classList.remove("green")
+    yellowc.classList.remove("yellow")
+});
+
+green.addEventListener('click',function(){
+    greenc.classList.add("green");
+    redc.classList.remove("red")
+    yellowc.classList.remove("yellow")
+});
+
+yellow.addEventListener('click',function(){
+    yellowc.classList.add("yellow");
+    redc.classList.remove("red")
+    greenc.classList.remove("green")
+});
+
