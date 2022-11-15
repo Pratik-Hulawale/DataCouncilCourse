38d5b90095dc7f51c93e34920aa4cc08685d72ff
pratik-1847 <83464018+pratik-1847@users.noreply.github.com>
2022-11-15 18:07:45 +0530

    F

---
+++ New folder/index.js
@@ -0,0 +1,20 @@
+let even=(numberStart, numberEnd)=>{
+    for(let i=numberStart;i<=numberEnd;i++){
+        if(i%2==0){
+            console.log(i);
+        }
+    }
+}
+
+even(1000,1002)
+
+
+let arr="String";
+let op=" "
+
+for(let i=arr.length;i<0;i--){
+
+
+}
+
+console.log(arr.length)
